57f3f5fdd5bc018cf328e641dcfd6e4ea70012b0
erdalibis75-dotcom <erdalibis75@gmail.com>
2026-04-11 13:45:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,7 @@ const CACHE_NAME = 'cuzdan-pro-v1';
 const FILES_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json',
-  
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
